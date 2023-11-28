41b4a3ce1d33523d5b4b6cb3cc946def68f07729
Rodrigo Medeiros <rodrigovinicius.bsb@gmail.com>
2023-11-28 14:38:29 -0300

    Feat: add dashboard page and react router dom

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-
-export const App = () => {
-  return (
-    <div className="App">
-     Olá
-    </div>
-  );
-}
-
---
+++ src/app/routes/index.tsx
@@ -0,0 +1,19 @@
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes as Switch,
+} from "react-router-dom";
+import { Dashboard } from "../pages";
+
+export const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/pagina-inicial" element={<Dashboard />}/> 
+
+        <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
